97a56a496dfc1b4f5f7bbe470e7cf16b7c5ec366
tfedorov <t.fedorov.s@gmail.com>
2020-12-30 15:36:50 +0200

    Add FacebookStreamTest

---
+++ src/test/scala/com/tfedorov/inteview/daily/FacebookStreamTest.scala
@@ -0,0 +1,26 @@
+package com.tfedorov.inteview.daily
+
+import org.junit.jupiter.api.Assertions.assertTrue
+
+import scala.util.Random
+
+import org.junit.jupiter.api.Test
+/*
+This problem was asked by Facebook.
+
+Given a stream of elements too large to store in memory, pick a random element from the stream with uniform probability.
+ */
+class FacebookStreamTest {
+
+  @Test
+  def streamTest(): Unit = {
+    val random = new Random()
+
+    val stream = Stream.from(0)
+    val element2Find = random.nextInt(10000)
+
+    val actualResult: String = stream.view(element2Find, element2Find + 1).map { i => println(i); "" + i + random.nextPrintableChar() + random.nextPrintableChar() }.head
+
+    assertTrue(actualResult.startsWith("" + element2Find))
+  }
+}
