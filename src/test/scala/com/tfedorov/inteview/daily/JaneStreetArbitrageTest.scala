8c65a6fcce6c35626c1bb72473c75c5ac15765e5
tfedorov <t.fedorov.s@gmail.com>
2021-01-18 19:31:08 +0200

    Add MicrosoftMedTest

---
+++ src/test/scala/com/tfedorov/inteview/daily/JaneStreetArbitrageTest.scala
@@ -0,0 +1,19 @@
+package com.tfedorov.inteview.daily
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class JaneStreetArbitrageTest {
+  @Test
+  def defaultTest(): Unit = {
+    val inputArr = Seq(
+      Seq("ARS","AUD", 0.036093121),
+      Seq("AUD","ARS", 0.036093121),
+    )
+
+    val actualResult = "1"
+
+    val expectedResult = "2"
+    assertEquals(expectedResult, actualResult)
+  }
+}
