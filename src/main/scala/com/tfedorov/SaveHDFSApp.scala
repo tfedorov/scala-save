a50b442a350a9c19d466570024decf609c0c9cea
tfedorov <t.fedorov.s@gmail.com>
2021-01-20 19:00:03 +0200

    fixes

--- src/main/scala/com/tfedorov/SaveHDFSApp.scala
+++ src/main/scala/com/tfedorov/SaveHDFSApp.scala
@@ -9,16 +9,15 @@ object SaveHDFSApp extends App {
   val conf = new Configuration()
   conf.set("fs.defaultFS", "hdfs://localhost:9000")
   conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem")
-  //conf.set("dfs.client.use.datanode.hostname", "true")
   val fs = FileSystem.get(conf)
 
   def mkPath(str: String): Path = new Path(str)
 
-  val localSource = mkPath("/Users/tfedorov/IdeaProjects/tmp/pod.yaml")
+  val localSource = mkPath("/Users/tfedorov/IdeaProjects/tmp/tmp.txt")
   val hdfsDest = mkPath("/Hdfs3SinkConnector")
-  // fs.copyFromLocalFile(localSource, hdfsDest)
+  //fs.copyFromLocalFile(localSource, hdfsDest)
 
   val localDest = mkPath("/Users/tfedorov/IdeaProjects/tmp/")
-  val hdfsSource = mkPath("/Hdfs3SinkConnector/pod.yaml")
+  val hdfsSource = mkPath("/Hdfs3SinkConnector/tmp.txt")
   fs.copyToLocalFile(hdfsSource, localDest)
 }
