67ee8abf73585939b37d80512adbd7f2e7e2fd62
tfedorov <t.fedorov.s@gmail.com>
2021-01-22 12:24:59 +0200

    refactor

--- src/test/scala/com/tfedorov/inteview/GD/MatrixRotateTest.scala
+++ src/test/scala/com/tfedorov/inteview/GD/MatrixRotateTest.scala
@@ -5,46 +5,47 @@ import org.junit.jupiter.api.Test
 
 class MatrixRotateTest {
 
-  def rotateMatrix(input: Array[Array[String]]): Array[Array[String]] = {
+  private type Matrix = Seq[Seq[String]]
+
+  private def rotateMatrix(input: Matrix): Matrix = {
 
     val width = input.head.length
-    val zero: Seq[Seq[String]] = Seq.fill(width)(Nil)
-    input.foldLeft(zero) { (resultAgg, inputRow) =>
-      resultAgg.zip(inputRow.reverse).map { case (resultRow, inputRowEl) =>
-        resultRow :+ inputRowEl
-      }
-    }.map(_.toArray).toArray
+    val zero: Matrix = Seq.fill(width)(Nil)
+    input.foldLeft(zero) { (result, inputRow) =>
+      result.zip(inputRow.reverse)
+        .map { case (resultRow, inputRowEl) =>  resultRow :+ inputRowEl }
+    }
   }
 
   @Test
   def defaultTest(): Unit = {
-    val input = Array(
-      Array("a", "b", "c"),
-      Array("e", "f", "g"),
-      Array("h", "i", "j"))
+    val input = Seq(
+      Seq("a", "b", "c"),
+      Seq("e", "f", "g"),
+      Seq("h", "i", "j"))
 
-    val actualResult: Array[Array[String]] = rotateMatrix(input)
+    val actualResult: Matrix = rotateMatrix(input)
 
-    val expectedResult = Array(
-      Array("c", "g", "j"),
-      Array("b", "f", "i"),
-      Array("a", "e", "h"))
-    assertEquals(expectedResult.map(_.toSeq).toSeq, actualResult.map(_.toSeq).toSeq)
+    val expectedResult = Seq(
+      Seq("c", "g", "j"),
+      Seq("b", "f", "i"),
+      Seq("a", "e", "h"))
+    assertEquals(expectedResult, actualResult)
   }
 
   @Test
   def customTest(): Unit = {
-    val input = Array(
-      Array("a", "b", "c"),
-      Array("e", "f", "g"),
+    val input = Seq(
+      Seq("a", "b", "c"),
+      Seq("e", "f", "g"),
     )
 
-    val actualResult: Array[Array[String]] = rotateMatrix(input)
+    val actualResult: Matrix = rotateMatrix(input)
 
     val expectedResult = Array(
       Array("c", "g"),
       Array("b", "f"),
       Array("a", "e"))
-    assertEquals(expectedResult.map(_.toSeq).toSeq, actualResult.map(_.toSeq).toSeq)
+    assertEquals(expectedResult, actualResult)
   }
 }
