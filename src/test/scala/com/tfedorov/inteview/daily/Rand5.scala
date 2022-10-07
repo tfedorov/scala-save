0bbb46442aaa402131ec4d78797a5d7ba25440d0
Taras Fedorov <tfedorov@apple.com>
2022-10-07 18:32:59 +0300

    Add OutOfOrder tasks

--- src/test/scala/com/tfedorov/inteview/daily/Rand5.scala
+++ src/test/scala/com/tfedorov/inteview/daily/Rand5.scala
@@ -14,11 +14,13 @@ class Rand5 {
   def rand(limit: Int): Int = {
     val divider = 5.0 / limit
     val fistRand = rand5()
-    if (fistRand / 5.0 < divider)
+    if (fistRand / 5.0 <= divider)
       return rand5()
 
-    val secondRand = (rand5() / 5.0 * (limit - 6)).toInt
-    6 + secondRand
+    val secondRand = rand5()
+    val s = secondRand / 5.0
+    val add = (s * (limit - 5) - 0.0000001).toInt
+    6 + add
   }
 
   def rand5(): Int = {
@@ -26,13 +28,24 @@ class Rand5 {
   }
 
   @Test
-  def randTest(): Unit = {
+  def rand7Test(): Unit = {
 
     val actualResults = (1 to 10000).map(_ => rand(7))
     println(actualResults.groupBy(identity).mapValues(i => i.length))
 
     assertEquals(1, actualResults.min)
     assertEquals(7, actualResults.max)
+
+  }
+
+  @Test
+  def rand9Test(): Unit = {
+
+    val actualResults = (1 to 10000).map(_ => rand(9))
+    println(actualResults.groupBy(identity).mapValues(i => i.length))
+
+    assertEquals(1, actualResults.min)
+    assertEquals(9, actualResults.max)
 
   }
 }
