f2461c4fcdb31cfa7a6c20e7b70820b4d0372d19
Taras Fedorov <tfedorov@apple.com>
2024-08-27 13:53:49 +0300

    rotated line. One day task

--- src/test/scala/com/tfedorov/TestBoilerPlot.scala
+++ src/test/scala/com/tfedorov/TestBoilerPlot.scala
@@ -7,8 +7,9 @@ class TestBoilerPlot {
 
   @Test
   def taskTest(): Unit = {
+    val input = 2
 
-    val actual = 2 + 2
+    val actual = input + 2
 
     val expected = 4
     assertEquals(expected, actual)
