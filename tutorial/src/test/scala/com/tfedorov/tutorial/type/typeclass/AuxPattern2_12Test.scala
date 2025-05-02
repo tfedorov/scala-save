8994aa0fbd4a6d5d32ac4f72e1289a451cd7af93
tfedorov <tfedorov@apple.com>
2025-05-02 17:48:17 +0300

    Minor changes

--- tutorial/src/test/scala/com/tfedorov/tutorial/type/typeclass/AuxPattern2_12Test.scala
+++ tutorial/src/test/scala/com/tfedorov/tutorial/type/typeclass/AuxPattern2_12Test.scala
@@ -15,21 +15,21 @@ object AuxPattern2_12Test {
       def find(value: A): Out
     }
 
-    object Finder {
-      // Implicit instance for Int → returns String
-      implicit val intFinder: Finder[Int] {type Out = String} = new Finder[Int] {
-        type Out = String
 
-        def find(value: Int): String = s"Found number: $value"
-      }
+    // Implicit instance for Int → returns String
+    implicit val intFinder: Finder[Int] {type Out = String} = new Finder[Int] {
+      type Out = String
 
-      // New conflicting implicit instance for Int → returns Int instead of String
-      implicit val intFinderAlt: Finder[Int] {type Out = Int} = new Finder[Int] {
-        type Out = Int
-
-        def find(value: Int): Int = value * 10
-      }
+      def find(value: Int): String = s"Found number: $value"
     }
+
+    // New conflicting implicit instance for Int → returns Int instead of String
+    implicit val intFinderAlt: Finder[Int] {type Out = Int} = new Finder[Int] {
+      type Out = Int
+
+      def find(value: Int): Int = value * 10
+    }
+
 
     // ❌ Now, implicit resolution fails due to ambiguity
     def process[A](value: A)(implicit finder: Finder[A]): finder.Out = {
